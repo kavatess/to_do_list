e770b6e18541ec12dc715b96aba22046413da958
TranVanDuc-16520 <90029085+TranVanDuc-16520@users.noreply.github.com>
2021-09-24 17:01:06 +0700

    add label and change input

--- js/sua_thong_tin.js
+++ js/sua_thong_tin.js
@@ -1,10 +1,28 @@
 function sua_thong_tin(index, chi_so) {
     var card_duoc_sua = danh_sach_lon[index].thong_tin[chi_so];
     var started_fix = `
-<input type="text" placeholder="${card_duoc_sua.job_name}" id="fix_job_name">
-<input type="text" placeholder="${card_duoc_sua.assigned_person}" id="fix_assigned_person">
-<input type="text" placeholder="${card_duoc_sua.start_date}" id="fix_start_date">
-<input type="text" placeholder="${card_duoc_sua.deadline}" id="fix_deadline">`
+    <div class="inline-block">
+        <label>Tên công việc</label>
+        <input type="text" placeholder="${card_duoc_sua.job_name}" id="fix_job_name">
+    </div>
+    <div class="inline-block">
+        <label>Người thực hiện</label>
+        <input type="text" placeholder="${card_duoc_sua.assigned_person}" id="fix_assigned_person">
+    </div>
+    <div class="inline-block">
+        <label>Thời gian bắt đầu</label>
+        <div>
+        <input type="date" id="fix_start_date">
+        <input type="time" id="fix_start_time">
+        </div>
+    </div>
+    <div class="inline-block">
+        <label>Thời gian hoàn thành</label>
+        <div>
+        <input type="date" id="fix_deadline">
+        <input type="time" id="fix_deadline_time">
+        </div>
+    </div>`
     content_modal_fix.innerHTML = started_fix;
     document.getElementById('fix_detail').setAttribute('thu_duoc_fix', index);
     document.getElementById('fix_detail').setAttribute('the_duoc_fix', chi_so);
